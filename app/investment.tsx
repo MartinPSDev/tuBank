b101e878d259cd6d18174062d2d245e3af52af46
M4rt1n_0x1337 <martinperezsotelo.dev@gmail.com>
2025-07-09 01:20:09 -0300

    Add app routes and navigation structure

---
+++ app/investment.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import InvestmentScreen from '../src/screens/InvestmentScreen';
+
+export default function InvestmentPage() {
+  return <InvestmentScreen />;
+}
---
+++ app/withdraw-amount.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import WithdrawAmountScreen from '../src/screens/WithdrawAmountScreen';
+
+export default function WithdrawAmountPage() {
+  return <WithdrawAmountScreen />;
+}
